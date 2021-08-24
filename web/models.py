0334baffd0d72974d811022d098587cab752f5d9
Olatunji <iamkomolafe.o.s@gmail.com>
2021-08-24 08:13:32 +0100

    created contact forms database model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=90, null= False, blank= True)
+    email = models.EmailField(max_length=90, null=False, blank= True)
+    subject = models.CharField(max_length=90, null=False, blank= True)
+    message = models.TextField(blank=True, null=False)
+
+    def __str__(self):
+        return self.name
