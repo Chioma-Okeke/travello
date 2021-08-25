7bd957b595e0f809bb35bf6a5098ce6f9cd265f2
chioma okeke <princesssophia784@gmail.com>
2021-08-25 12:43:59 +0100

    updated

--- authentication/views.py
+++ authentication/views.py
@@ -4,25 +4,22 @@ from django.http import HttpResponse
 from .forms import LoginForm
 
 
-# Create your views here.
-# def loginPage(request):
-# 	form = UserCreationForm()
-# 	context = {'form':form}
-# 	return render(request, '', context)
-
 
 # def RegisterPage(request):
 # 	form = UserCreationForm()
 # 	context = {'form':form}
 # 	return render(request, '', context)
+
+
 def user_login(request):
+	context = {}
 	if request.method == 'POST':
-		form = LoginFOrm(requrst.POST)
+		form = LoginForm(request.POST)
 		if form.is_valid():
 			cd = form.cleaned_data
 			user = authenticate(request, username=cd['username'], password=cd['password'])
 			if user is not None:
-				if user.is_actgive:
+				if user.is_active:
 					login(request, user)
 					return HttpResponse('Authenticated')
 				else:
@@ -31,4 +28,6 @@ def user_login(request):
 				return HttpResponse('Invalid login')
 		else:
 			form = LoginForm()
-		return render(request, '', {'form': form})
+			
+	context['form'] = form
+	return render(request, '', context)
